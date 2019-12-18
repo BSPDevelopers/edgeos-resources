895f1107fbc42d8545adcf203731db89142e2da9
BSPDevelopers <36006826+BSPDevelopers@users.noreply.github.com>
2019-12-18 10:27:55 -0800

    Update update-wireguard.sh

--- update-wireguard.sh
+++ update-wireguard.sh
@@ -15,7 +15,7 @@ cd $scripts
 curl -O https://raw.githubusercontent.com/BSPDevelopers/edgeos-resources/master/refresh-dns.sh > refresh-dns.sh
 
 cd $downloads
-curl -LJO https://github.com/BSPDevelopers/edgeos-resources/blob/master/wireguard-v2.0-e50.deb > wireguard.deb
+curl -LJO https://github.com/BSPDevelopers/edgeos-resources/raw/master/wireguard-v2.0-e50.deb > wireguard.deb
 
 for pkg in *; do
   dpkg-query -W --showformat='${Status}\n' $(dpkg --info "${pkg}" | grep "Package: " | awk -F' ' '{ print $NF}') > /dev/null 2>&1 || (dpkg -i ${pkg} && reboot now)
