cf57bb0dc64491e5177143fa3f032e6c0a061fa5
BSPDevelopers <36006826+BSPDevelopers@users.noreply.github.com>
2019-12-20 09:24:57 -0800

    Update refresh-dns.sh

--- refresh-dns.sh
+++ refresh-dns.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-hostIPAddress=$(sudo host test.test | sed -n "s/.*IPv4 address //p")
+hostIPAddress=$(sudo host ipsec.broadstreet.ca | sed -n "s/.*IPv4 address //p")
 endpoint=$(sudo wg show wg0 endpoints | sed -n "s/^.*\s//p" | sed -n "s/:.*$//p")
 PUBLIC_KEY=YQFBWrpLk49iKjhCAPOJkHlPI+iJFbn1XK87Boz+UEI=
-ENDPOINT=test.test:51820
+ENDPOINT=ipsec.broadstreet.ca:51820
 
 
 if [ "$hostIPAddress" == "$endpoint" ]
